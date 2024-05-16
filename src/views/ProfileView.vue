2b30dfb9782cd28504356e5b684440a5f35afc56
Sen. <senu.reply@gmail.com>
2024-05-16 20:39:24 +0200

    First Api Request using fetch

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,9 +1,29 @@
+<script>
+import axios from "axios";
+import { ref } from 'vue';
+
+export default {
+  name: "ProfileView",
+
+  setup(){
+    const result = ref(null)
+
+    fetch('http://localhost:3000/user')
+      .then(response => response.json())
+      .then(data => result.value = data)
+      
+    
+    return { result }
+  }
+}
+</script>
+
 <template>
   <div class="profile">
     <h1>This is an Profile page</h1>
+    <p>{{ result }}</p>
   </div>
 </template>
 
 <style>
-
 </style>
