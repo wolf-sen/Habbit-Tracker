3c8529b34abe4564fe2d3910cb82e36cdba53c84
Sen. <senu.reply@gmail.com>
2024-05-24 00:12:53 +0200

    Included Font-Familys (weight 400, 700)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['SF Pro Display'],
+        soft: ['SF Pro Rounded'],
+        body: ['SF Pro Text'],
+        mono: ['SF Mono']
+      },
+    },
   },
   plugins: [],
 }
